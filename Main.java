a6afa502bfa38f8ef9235a3f8038ef654b6bf5a3
mirage109 <111114242+mirage109@users.noreply.github.com>
2022-08-13 00:02:45 +0300

    Basic Java - 10.08.2022
    Создать новый проект написать. Написать программу, в которой задано 2 дробных числа. Программа должна выводить на экран результат сложения, вычитания, умножения и деления этих чисел. Например: задано 2 переменных: 10.6 и 2.2  в результате:
    10.6 + 2.2=12.8
    10.6 - 2.2=8.4
    10.6 *2.2=23.32
    10.6 / 2.2=4.818181

--- Main.java
+++ Main.java
@@ -2,9 +2,9 @@ public class Main {
     public static void main(String[] args) {
         double first = 15.5;
         double second = 22.3;
-        System.out.println("Сумма чисел 15.5 и 22.3 будет "+ (first+second));
-        System.out.println("Реультат вычитания чисел 15.5 и 22.3 будет "+ String.format("%5.2f",(first-second)));
-        System.out.println("Реультат умножения чисел 15.5 и 22.3 будет "+ String.format("%5.2f",(first*second)));
-        System.out.println("Реультат деления чисел 15.5 и 22.3 будет"+ String.format("%5.2f",(first/second)));
+        System.out.println("РЎСѓРјРјР° С‡РёСЃРµР» 15.5 Рё 22.3 Р±СѓРґРµС‚ "+ (first+second));
+        System.out.println("Р РµСѓР»СЊС‚Р°С‚ РІС‹С‡РёС‚Р°РЅРёСЏ С‡РёСЃРµР» 15.5 Рё 22.3 Р±СѓРґРµС‚ "+ String.format("%5.2f",(first-second)));
+        System.out.println("Р РµСѓР»СЊС‚Р°С‚ СѓРјРЅРѕР¶РµРЅРёСЏ С‡РёСЃРµР» 15.5 Рё 22.3 Р±СѓРґРµС‚ "+ String.format("%5.2f",(first*second)));
+        System.out.println("Р РµСѓР»СЊС‚Р°С‚ РґРµР»РµРЅРёСЏ С‡РёСЃРµР» 15.5 Рё 22.3 Р±СѓРґРµС‚"+ String.format("%5.2f",(first/second)));
     }
 }
