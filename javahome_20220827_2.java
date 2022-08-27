9ad4f105b528552dc739012beaa2842e479e9bb7
mirage109 <111114242+mirage109@users.noreply.github.com>
2022-08-27 23:52:24 +0300

    27.08.2022_2
           //превышение менее 10% от скоростного ограничения – не штрафуется
    
            //10% - менее 20%   от скоростного ограничения – 50 евро
    
            //20% - менее 30%   от скоростного ограничения – 150 евро не на автобане, и 200 на автобане
    
            //от 30% и выше  - 500евро плюс, если нарушение на автобане, лишение прав на 3 месяца.
            //Реализуйте программу, которая рассчитывает штраф и выводит соответствующее сообщение на экран.

---
+++ javahome_20220827_2.java
@@ -0,0 +1,34 @@
+public class Main {
+    public static void main(String[] args) {
+        //превышение менее 10% от скоростного ограничени€ Ц не штрафуетс€
+
+        //10% - менее 20%   от скоростного ограничени€ Ц 50 евро
+
+        //20% - менее 30%   от скоростного ограничени€ Ц 150 евро не на автобане, и 200 на автобане
+
+        //от 30% и выше  - 500евро плюс, если нарушение на автобане, лишение прав на 3 мес€ца.
+        //–еализуйте программу, котора€ рассчитывает штраф и выводит соответствующее сообщение на экран.
+
+        double speedAllowed = 100;
+        double carSpeed = 150;
+        boolean autoban = true;
+        double tenPercent = 1.1;
+        double twentyPercent = 1.2;
+        double thirtyPercent = 1.3;
+        if (carSpeed < (speedAllowed * tenPercent)) {
+            System.out.println("Don't go so fast!");
+        } else if ((carSpeed < speedAllowed * twentyPercent) && (carSpeed >= speedAllowed * tenPercent)) {
+            System.out.println("Please, pay 50 Euro");
+        } else if ((carSpeed < speedAllowed * thirtyPercent) && (carSpeed >= speedAllowed * twentyPercent) && !autoban) {
+            System.out.println("Please, pay 150 Euro");
+        } else if ((carSpeed < speedAllowed * thirtyPercent) && (carSpeed >= speedAllowed * twentyPercent) && autoban) {
+            System.out.println("Please, pay 200 Euro");
+        } else if (carSpeed >= speedAllowed * thirtyPercent && !autoban) {
+            System.out.println("Please, pay 500 Euro");
+        } else if (carSpeed >= speedAllowed * thirtyPercent && autoban) {
+            System.out.println("Please, give your drive license and 500 euro.");
+        }
+    }
+
+}
+
