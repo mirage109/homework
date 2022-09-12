be4b1c397e163a65a1f5de1c7c104459f00209c2
Anastasiia Zibla <111114242+mirage109@users.noreply.github.com>
2022-09-12 09:34:52 +0200

    task Java homework 20220912

---
+++ Program.java
@@ -0,0 +1,25 @@
+public class Program {
+    public static void main(String[] args) {
+        counting();
+        summa();
+    }
+
+    static void counting() {
+        //First level: C помощью цикла for написать метод, печатающий цифры от 1 до  числа  n
+        int n = 15;
+        for (int i = 0; i <= n; i += 1) {
+            System.out.println(i + " ");
+        }
+    }
+
+    static void summa() {
+        //C помощью цикла for написать метод возвращающий сумму цифр от 1 до числа n
+        int num = 10;
+        int y;
+        int result = 0;
+        for (y = 0; y <= num; y += 1) {
+            result += y;
+        }
+        System.out.println("Sum of numbers from 1 to 10 is " + result);
+    }
+}
